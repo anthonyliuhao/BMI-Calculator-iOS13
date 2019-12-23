12410c93778f60fcc8264023c4a4eb7f1c967eef
Anthony Liu <anthonyliuhao@icloud.com>
2019-12-22 23:22:39 -0800

    Display values as user drags the sliders

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    @IBAction func heightSliderDragged(_ sender: UISlider) {
+        let roundedValue = round(100.0 * sender.value) / 100.0
+        heightLabel.text = "\(roundedValue)m"
+    }
+    
+    
+    @IBAction func weightSliderDragged(_ sender: UISlider) {
+        weightLabel.text = "\(Int(sender.value))Kg"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
