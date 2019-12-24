d6dd06010978b590b9ba2c0417190f5f121581ea
Anthony Liu <anthonyliuhao@icloud.com>
2019-12-23 22:21:40 -0800

    Calculate and print the BMI

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -11,22 +11,33 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
     
     @IBAction func heightSliderDragged(_ sender: UISlider) {
         let roundedValue = round(100.0 * sender.value) / 100.0
         heightLabel.text = "\(roundedValue)m"
     }
     
-    
     @IBAction func weightSliderDragged(_ sender: UISlider) {
         weightLabel.text = "\(Int(sender.value))Kg"
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = round(100.0 * heightSlider.value) / 100.0
+        let weight = weightSlider.value
+        
+        let bmi = weight / (height * height)
+        
+        print(bmi)
+    }
+    
 
 }
 
