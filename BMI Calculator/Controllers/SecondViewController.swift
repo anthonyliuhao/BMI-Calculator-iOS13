f263683722c24be42a07083db36aae23ee24f8bd
Anthony Liu <anthonyliuhao@icloud.com>
2019-12-23 23:46:31 -0800

    Adding a second view controller

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Anthony Liu on 2019/12/23.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
